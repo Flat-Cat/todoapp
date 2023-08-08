011a9dd4e74671d1b334643ba8b448db7b0f652e
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-08-08 18:16:12 +0200

    chanced home side. added design and 2 buttons

--- reactgame/src/pages/home.tsx
+++ reactgame/src/pages/home.tsx
@@ -1,15 +1,63 @@
 import "./home.scss";
 //import { Link } from "react-router-dom";
 
+
+
+
 function Home() {
-   
+    const onContact = () => {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    };
+    // HomePictures
     return (
+        <div className="Home ">
 
-<div className="button">Text Button</div>
+            <section className="home-introduction has-background-white-ter pt-6">
+                <div className="container px-4">
 
-    )
+{/*__________columns__________*/}
+                    <div className="columns my-0 ">
+                        <div className="column is-6 py-0">
+
+                            <h2 className="title is-2">
+                                Hier ensteht ein Spiel
+                            </h2>
+
+                            <h5 className="subtitle is-5">
+                                Fortsetzung folgt...
+                            </h5>
+                            {/* Button */}
+
+                            <button
+                                className="button is-small is-outlined is-outlined mr-5 mt-5 my-5"
+                                onClick={onContact}>
+                                Meine Webseite
+                            </button>
+
+                            <a
+                                href="https://github.com/Flat-Cat/"
+                                target="_blank"
+                                className="button is-small is-outlined mt-5 my-5">
+                                GitHub
+                            </a>
+                        </div>
+
+                        <div className="column is-6 py-0 pt-4">
+
+<h2 className="title"> :) </h2>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </section>
+            <section className="home-skills">
+
+                {/*x Hier farbe ändern */}
+
+            </section>
+        </div>
+    );
 }
-
-
-
-export default Home
+export default Home;
