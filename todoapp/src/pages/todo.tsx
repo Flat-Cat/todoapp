446d4db7f645a6492d5f75daf544dc7028da40b9
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-08-16 14:01:29 +0200

    cleaned code. moved buttons to the right

--- todoapp/src/pages/todo.tsx
+++ todoapp/src/pages/todo.tsx
@@ -16,17 +16,16 @@ function Todo() {
             const value = target.value;
             setTodoList([...todoList, value]);
             setTodo("")
-            //  ... = spread operator == 2 arrays werden zusammengefügt#
+            //  ... = spread operator = 2 arrays werden zusammengefügt
         }
     };
-    //__________________________________________changeInput___________________________________________________
+    //________________________________changeInput_____________________________________________________________
     const changeInput = function (e: ChangeEvent<HTMLInputElement>) {
         const target = e.target as HTMLInputElement;
         setTodo(target.value);
     };
     //_______________________________return___________________________________________________________________
     return (
-
         <div className="Todo ">
             <section className="todo-introduction has-background-white-ter pt-6">
                 <div className="container px-4">
@@ -35,7 +34,7 @@ function Todo() {
                         <div className="control pl-6 pr-6 pb-6 pt-6 has-text-left">
                             <div className="is-size-6">My Todo:</div>
 
-                            {/*__________Input Todo: das TARGET_________*/}
+                            {/*_____________________Input Todo: das TARGET_______________________*/}
                             <input
                                 className="input"
                                 name="todo"
@@ -48,12 +47,13 @@ function Todo() {
                             </input>
 
                             <ul className="has-left">
-                                {todoList.map((todo, index) => (
-                                    <li key={index}>
-                                        {index + 1}. {todo}
-                                        <button className="delete is-medium has-right"></button>
-                                    </li>
-                                ))}
+                                {todoList.map((todo, index) => (<li key={index}> {index + 1}. {todo}
+                                    
+                                    <div className="buttons is-right">
+                                        <button className="delete is-medium"></button>
+                                    </div>
+                                </li>))}
+
                             </ul>
 
                             {/* todo ohne klammer = ein param. todo in klammern (todo) kann nun 2 parameter geben, da ein array natürlich noch ein index besitzt */}
